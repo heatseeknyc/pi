bb0bf4bb0c06d04d269b04bef604c4e83ac2b755
William Jeffries <wm.jeffries@gmail.com>
2015-07-19 16:27:39 -0400

    Remove sudo when creating ssh keys

--- setup.sh
+++ setup.sh
@@ -22,8 +22,11 @@ sudo pip-3.2 install -Ur requirements.txt
 mkdir -p ~/.ssh
 cat conf/relay_rsa.pub >> ~/.ssh/authorized_keys
 
-sudo ssh-keygen # TODO how to statically build this in to Heat Seek OS without compromising relay server?
-sudo ssh-copy-id hubs@relay.heatseeknyc.com
+# TODO how to statically build this in to Heat Seek OS without compromising relay server?
+# NOTE: consider imaging the pi post-setup and distributing the fully baked version
+# NOTE: don't use sudo to create the ssh keys as it will place them in /root/ with restrictive permissions
+ssh-keygen
+ssh-copy-id hubs@relay.heatseeknyc.com
 
 sudo ln -sf $PWD/conf/wvdial.conf /etc/
 sudo sed -i 's/^usepeerdns/# usepeerdns/' /etc/ppp/peers/wvdial
