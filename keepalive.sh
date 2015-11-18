37deb51970a046e9ff4fca7dbfebea6800c34b0a
Harold Cooper <hrldcpr@gmail.com>
2015-11-18 18:29:55 -0500

    before rebooting to fix internet, kill usb for 10 seconds, maybe it will knock some sense into the modem…

--- keepalive.sh
+++ keepalive.sh
@@ -3,7 +3,10 @@ while true; do
     if ! ping -c 1 relay.heatseeknyc.com; then
         if ! ping -c 1 google.com; then
             if (( ++failures >= 1000 )); then  # every 17 hours
-                echo "$failures failures, rebooting."
+                echo "$failures failures, killing usb."
+                echo 0 > /sys/devices/platform/soc/20980000.usb/buspower
+                sleep 10
+                echo "rebooting."
                 shutdown -r now
             elif (( ++failures % 10  == 0 )); then  # every 10 minutes
                 echo "$failures failures, killing wvdial."
