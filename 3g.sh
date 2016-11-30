da7a8c4164d703009d1a304593f995f8d966f177
Ryan Boland <bolandryanm@gmail.com>
2016-11-30 11:59:10 -0500

    bypass modem setup for 15 seconds if there is already internet access (for dev with ethernet)

--- 3g.sh
+++ 3g.sh
@@ -1,17 +1,23 @@
 
 while true; do
-    echo 'unbinding USB...'
-    echo '1-1' > /sys/bus/usb/drivers/usb/unbind
-    sleep 5
-    echo 'binding USB...'
-    echo '1-1' > /sys/bus/usb/drivers/usb/bind
-    sleep 15
-    # command copied from usb_modeswitch startup log in debug mode:
-    usb_modeswitch --no-inquire --verbose --sysmode \
-                   --check-success 20 --configuration -1 \
-                   --default-vendor 12d1 --default-product 1446 \
-                   -c conf/usb_modeswitch.conf
-    sleep 10
+    if ! ping -c 1 google.com; then
+        echo 'unbinding USB...'
+        echo '1-1' > /sys/bus/usb/drivers/usb/unbind
+        sleep 5
+        echo 'binding USB...'
+        echo '1-1' > /sys/bus/usb/drivers/usb/bind
+        sleep 15
+        # command copied from usb_modeswitch startup log in debug mode:
+        usb_modeswitch --verbose --sysmode \
+                       --check-success 20 --configuration -1 \
+                       --default-vendor 12d1 --default-product 1446 \
+                       -c conf/usb_modeswitch.conf
+        sleep 10
 
-    wvdial
+        wvdial
+    fi
+
+    echo 'already connected to internet (without modem)'
+
+    sleep 15
 done
