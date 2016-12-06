8ddb9447e8c7228f2fd25ee09452590743f0e33b
Ryan Boland <bolandryanm@gmail.com>
2016-12-06 11:29:09 -0500

    unconditionally check for commands

--- hub/receive_commands.py
+++ hub/receive_commands.py
@@ -14,20 +14,19 @@ def main():
         logging.info('connected to database.')
 
         while True:
-            if db.get_sleep_period() == 1:
-                xbee_id = common.hexlify(db.get_xbee_id())
+            xbee_id = common.hexlify(db.get_xbee_id())
 
-                response = requests.get('http://relay.heatseek.org/hubs/{}/commands'.format(xbee_id))
+            response = requests.get('http://relay.heatseek.org/hubs/{}/commands'.format(xbee_id))
 
-                if response.status_code == 200:
-                    for command in response.json():
-                        action, param = command
-                        if action == 'change_sleep_period':
-                            sleep_period = int(param)
-                            logging.error('updating sleep period to %s', sleep_period)
-                            xbee.update_sleep_period(sleep_period)
-                            break
-                else:
-                    logging.error('bad response: %s', response)
+            if response.status_code == 200:
+                for command in response.json():
+                    action, param = command
+                    if action == 'change_sleep_period':
+                        sleep_period = int(param)
+                        logging.error('updating sleep period to %s', sleep_period)
+                        xbee.update_sleep_period(sleep_period)
+                        break
+            else:
+                logging.error('bad response: %s', response)
 
-            time.sleep(10)
+            time.sleep(20)
